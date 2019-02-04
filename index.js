adfcdcfb4daef5c1b457542005c178bbfe5208da
Silla Tan <silla.tan@acloud.guru>
2019-02-04 14:20:19 +1100

    resolve tableName after constructor to obtain interpolated value

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ module.exports = class ServerlessDynamodbParameters {
   constructor(serverless) {
     this.serverless = serverless;
     this.provider = this.serverless.getProvider('aws'); // only allow plugin to run on aws
+    AWS.config.update({
+      region: this.provider.getRegion()
+    });
 
     const originalGetValueFromSource = serverless.variables.getValueFromSource.bind(serverless.variables);
     const originalTrackerAdd = serverless.variables.tracker.add.bind(serverless.variables.tracker);
@@ -24,22 +27,14 @@ module.exports = class ServerlessDynamodbParameters {
   }
 
   getDocumentClient() {
-    AWS.config.update({ region: this.provider.getRegion() });
-    return this.getTableName()
-    .then(tableName => new AWS.DynamoDB.DocumentClient({
+    const tableName = get(this.serverless, 'service.custom.serverless-dynamodb-parameters.tableName');
+
+    return new AWS.DynamoDB.DocumentClient({
       params: {
         TableName: tableName
       },
       convertEmptyValue: true
-    }));
-  }
-
-  getTableName() {
-    const rawTableName = get(this.serverless, 'service.custom.serverless-dynamodb-parameters.tableName');
-    return this.serverless.variables.getValueFromSource('opt:stage', rawTableName)
-    .then(stage => {
-      return rawTableName.replace(new RegExp('\\bopt:stage\\b'), stage);
-    })
+    });
   }
 
   getValueFromDdb(variableString) {
@@ -58,8 +53,8 @@ module.exports = class ServerlessDynamodbParameters {
       }
     };
 
-    return this.getDocumentClient()
-    .then(documentClient => documentClient.query(params).promise())
+    const documentClient = this.getDocumentClient();
+    return documentClient.query(params).promise()
     .then(res => {
       const value = get(res, 'Items.0.value');
 
